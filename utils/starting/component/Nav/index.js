223a3e1d09c5dc1d4d86be52dfbb56f15d4fcbf7
nassermaronie <maronienasser@gmail.com>
2017-03-18 16:08:41 +0700

    config starting

---
+++ utils/starting/component/Nav/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './style.scss';
+
+const Nav = (props) => {
+  return (
+    <div class={styles['Nav']}>
+      {/* content ... */}
+
+    </div>
+  );
+};
+
+export default Nav;
