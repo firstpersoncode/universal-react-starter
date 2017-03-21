756c45aae4719d2bec77192958027efd848efab0
nassermaronie <maronienasser@gmail.com>
2017-03-21 16:18:50 +0700

    fix bug on importing databases

---
+++ utils/starting/client/component/Footer/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './style.scss';
+const Footer = (props) => {
+  return (
+    <footer class={styles.Footer}></footer>
+  );
+};
+
+export default Footer;
