2a0a60eaa29097fb71ef72cd0f97aac96e3b85f2
nassermaronie <maronienasser@gmail.com>
2017-04-25 10:21:12 +0700

    update source for rendering, add preload on production environtment

--- utils/starting/server/source/index.js
+++ utils/starting/server/source/index.js
@@ -1,3 +1,4 @@
+import request from 'request';
 import React from "react";
 import { StaticRouter as Router, matchPath } from 'react-router';
 import { Provider } from 'react-redux';
@@ -8,16 +9,11 @@ import configureStore from "../../client/App/store";
 import Routes from "../../client/App/routers";
 import routes from "../../client/routes";
 
-
 export default (req, res) => {
-
-  // grab route object from ../../client/routes that match with req.url
   const match = routes.reduce((acc, route) => matchPath(req.url, route, { exact: true }) || acc, null);
-
   const store = configureStore();
   const context = {};
-  let html;
-  let body = (
+  const body = (
     <Provider store={store}>
       <Router
         location={req.url}
@@ -26,73 +22,62 @@ export default (req, res) => {
       </Router>
     </Provider>
   );
+  const html = ({ body, head, initialState }) => {
+      return `<!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="utf-8">
+          <meta http-equiv="x-ua-compatible" content="ie=edge">
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+          ${head.title.toString()}
+          ${head.meta.toString()}
+          ${head.link.toString()}
+          ${head.script.toString()}
+          ${process.env.NODE_ENV === "production" ? `
+          <link rel="stylesheet" type="text/css" href="/stylesheets/loading.css" />
+          <link rel="stylesheet" href="/stylesheets/style.css" />
+          ` : `
+          <link rel="stylesheet" href="http://localhost:50044/style.css" />
+          `}
+        </head>
+        <body>
+        ${process.env.NODE_ENV === "production" ? `
+          <div id="loading-bar">
+            <div id="progress">0%</div>
+          </div>
+        ` : ''}
+          <div id="root">${body}</div>
+          <script type="text/javascript">window.INITIAL_STATE = ${JSON.stringify(initialState)};</script>
+          ${process.env.NODE_ENV === "production" ? `
+          <script async src="/javascripts/loading.js"></script>
+          <script async src="/javascripts/bundle.js"></script>
+          ` : `
+          <script async src="http://localhost:50044/bundle.js"></script>
+          `}
+        </body>
+      </html>`;
+  };
 
-  if (process.env.NODE_ENV === "development") {
-    html = ({ body, head, initialState }) => {
-        return `<!DOCTYPE html>
-        <html lang="en">
-          <head>
-            <meta charset="utf-8">
-            <meta http-equiv="x-ua-compatible" content="ie=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-            ${head.title.toString()}
-            <link rel="stylesheet" href="http://localhost:50044/style.css" />
-            ${head.meta.toString()}
-            ${head.link.toString()}
-            ${head.script.toString()}
-          </head>
-          <body>
-            <div id="root">${body}</div>
-            <script>window.INITIAL_STATE = ${JSON.stringify(initialState)};</script>
-            <script src="http://localhost:50044/bundle.js"></script>
-          </body>
-        </html>`;
-    };
-  } else {
-    html = ({ body, head, initialState }) => {
-        return `<!DOCTYPE html>
-        <html lang="en">
-          <head>
-            <meta charset="utf-8">
-            <meta http-equiv="x-ua-compatible" content="ie=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-            ${head.title.toString()}
-            <link rel="stylesheet" href="stylesheets/style.css" />
-            ${head.meta.toString()}
-            ${head.link.toString()}
-            ${head.script.toString()}
-          </head>
-          <body>
-            <div id="root">${body}</div>
-            <script>window.INITIAL_STATE = ${JSON.stringify(initialState)};</script>
-            <script src="javascripts/bundle.js"></script>
-          </body>
-        </html>`;
-    };
+  const render = (output, helm, state) => {
+    return html({
+      body: output,
+      head: helm.rewind(),
+      initialState: state
+    })
   }
 
-  // Async initialState reducer and render the view
   store.renderUniversal(renderToString, body)
   .then(({ output }) => {
     const state = store.getState();
-    if (!match) {
-      res.status(404).send(html({
-        body: output,
-        head: Helm.rewind(),
-        initialState: state
-      }));
+    if (match) {
+      res.status(200).send(render(output, Helm, state));
     } else {
-      res.status(200).send(html({
-        body: output,
-        head: Helm.rewind(),
-        initialState: state
-      }));
+      res.status(404).send(render(output, Helm, state));
     }
   })
   .catch(({ output, error }) => {
     console.warn(error);
-    res.status(500).send(html({
-      body: error,
-    }));
+    const state = store.getState();
+    res.status(500).send(render(output, Helm, state));
   })
 };
